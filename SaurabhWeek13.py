c8e3d8ca993d4e8409f14d6b1ee5b54b499802e2
pisalsaurabh <35280751+pisalsaurabh@users.noreply.github.com>
2018-04-18 23:51:32 -0500

    created python file

---
+++ SaurabhWeek13.py
@@ -0,0 +1,15 @@
+class SaurabhWeek13():
+    def extract(self):
+        file1 = open("1950.txt", "r")
+        file2 = open("extracted_file.txt", "w")
+        print("Reading values from the file and printing them")
+        for pointer in file1:
+            longitude = pointer[34:41]
+            qualitycode = pointer[63]
+            temperature = pointer[87:92]
+            print()
+            file2.write(longitude + "\t" + temperature + "\t" + qualitycode + "\n")
+
+if __name__ == '__main__':
+    SaurabhWeek13.run()
+
